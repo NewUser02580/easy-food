8539911bbd0b0274ef5cc5b836e30efd332b5af5
Dhir Shah <shahdhir08@gmail.com>
2026-04-17 07:53:09 +0530

    Updated MongoDB connection and backend config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB =  async () => {
-    await mongoose.connect('mongodb+srv://202307030004_db_user:dhirshah11@cluster0.zrasxfu.mongodb.net/?appName=Cluster0').then(()=>console.log("DB Connected"));
+    await mongoose.connect(' mongodb+srv://202307030004_db_user:dhirshah11@cluster0.8hepl3a.mongodb.net/?appName=Cluster0')
+    .then(()=>console.log("DB Connected"));
 }
