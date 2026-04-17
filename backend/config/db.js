15641e3e3d82f68e17443868f432b4f312ad48b7
Dhir Shah <shahdhir08@gmail.com>
2026-04-17 17:01:18 +0530

    Updated MongoDB connection and backend config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB =  async () => {
-    await mongoose.connect('mongodb+srv://easyfood:dhirshah11@cluster0.zvme33u.mongodb.net/? ')
+    await mongoose.connect('mongodb+srv://easyfood:dhirshah11@cluster0.zvme33u.mongodb.net/food_del ')
     .then(()=>console.log("DB Connected"));
 }
