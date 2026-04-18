13eb2dffdbe8b29e268a46f1303b11ee24f8b890
Dhir Shah <shahdhir08@gmail.com>
2026-04-18 17:22:47 +0530

    fix cloudinary config

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -9,10 +9,13 @@ cloudinary.config({
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "easyfood",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "easyfood",
+      format: "jpg",
+      public_id: Date.now() + "-" + file.originalname.split(".")[0],
+    };
   },
 });
 
