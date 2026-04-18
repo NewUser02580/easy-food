9bb2126c8128eb3cfcf8c2acebbb778e7e9961a3
Dhir Shah <shahdhir08@gmail.com>
2026-04-18 11:00:23 +0530

    change in admin folder

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import './Sidebar.css'
-import { assets } from '../../assets/assets'
 import { NavLink } from 'react-router-dom'
+import { FaHome, FaPlus, FaList, FaBox } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar' >
+    <div className='sidebar'>
       <div className="sidebar-options">
-        <NavLink to='/dashboard' className="sidebar-option">
-            <p>Dashboard</p>
+
+        <NavLink to="/dashboard" className={({isActive}) => isActive ? "sidebar-option active" : "sidebar-option"}>
+          <FaHome />
+          <p>Dashboard</p>
         </NavLink>
-        <NavLink to='/add' className="sidebar-option">
-            <img src={assets.add_icon} alt="" />
-            <p>Add Items</p>
+
+        <NavLink to="/add" className={({isActive}) => isActive ? "sidebar-option active" : "sidebar-option"}>
+          <FaPlus />
+          <p>Add Items</p>
         </NavLink>
-        <NavLink to='/list' className="sidebar-option">
-            <img src={assets.order_icon} alt="" />
-            <p>List Items</p>
+
+        <NavLink to="/list" className={({isActive}) => isActive ? "sidebar-option active" : "sidebar-option"}>
+          <FaList />
+          <p>List Items</p>
         </NavLink>
-        <NavLink to='/orders' className="sidebar-option">
-            <img src={assets.order_icon} alt="" />
-            <p>Orders</p>
+
+        <NavLink to="/orders" className={({isActive}) => isActive ? "sidebar-option active" : "sidebar-option"}>
+          <FaBox />
+          <p>Orders</p>
         </NavLink>
+
       </div>
     </div>
   )
