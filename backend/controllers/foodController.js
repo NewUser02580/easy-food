305d75b4c30bbdd7b77b54c1ebc9ba7410e93be1
Dhir Shah <shahdhir08@gmail.com>
2026-04-18 22:36:33 +0530

    server changes

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -10,7 +10,7 @@ const addFood = async (req, res) => {
         description: req.body.description,
         price: req.body.price,
         category: req.body.category,
-        image: req.file ? req.file.originalname : "uploads",
+        image: image_filename,
       });
       await food.save();
       res.json({ success: true, message: "Food Added" });
@@ -35,16 +35,18 @@ const listFood = async (req, res) => {
 
 const removeFood = async (req, res) => {
   try {
-    let userData = await userModel.findById(req.userId);
+    let userData = await userModel.findById(req.body.userId);
     if (userData && userData.role === "admin") {
+      const food = await foodModel.findById(req.body.id);
+      fs.unlink(`uploads/${food.image}`, () => {});
       await foodModel.findByIdAndDelete(req.body.id);
       res.json({ success: true, message: "Food Removed" });
     } else {
       res.json({ success: false, message: "You are not admin" });
     }
   } catch (error) {
-    console.log("removeFood error:", error.message);
-    res.json({ success: false, message: error.message });
+    console.log(error);
+    res.json({ success: false, message: "Error" });
   }
 };
 
